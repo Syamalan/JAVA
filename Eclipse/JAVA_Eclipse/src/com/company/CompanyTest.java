e96064dfeb7b6b5f8a5430b262810c8020aa32d5
Syamalan <syamala.nuthalapati@gmail.com>
2025-09-23 17:08:44 +0530

    Employee and Account packages

---
+++ Eclipse/JAVA_Eclipse/src/com/company/CompanyTest.java
@@ -0,0 +1,14 @@
+package com.company;
+import com.company.accounts.*;
+import com.company.hr.*;
+
+public class CompanyTest {
+	public static void main(String[] args) {
+		Account a= new Account();
+		a.displayAccountDetails();
+		Employee e = new Employee();
+		e.displayEmployeeDetails();
+		
+	}
+
+}
---
+++ Eclipse/JAVA_Eclipse/src/com/company/accounts/Account.java
@@ -0,0 +1,7 @@
+package com.company.accounts;
+
+public class Account {
+	public void displayAccountDetails() {
+		System.out.println("Shows Account Details");
+	}
+}

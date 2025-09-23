df52b177903abf0f7ec6a6acc9da47978259a9e9
Syamalan <syamala.nuthalapati@gmail.com>
2025-09-23 17:10:02 +0530

    Accessing packages with public and private classes

---
+++ Eclipse/JAVA_Eclipse/src/com/example/access/PackagePrivateClass.java
@@ -0,0 +1,8 @@
+package com.example.access;
+
+public class PackagePrivateClass {
+	 void showPackagePrivateMessage() {
+		 System.out.println("package-private method cannot be accessed outside its package");
+	 }
+
+}
---
+++ Eclipse/JAVA_Eclipse/src/com/example/access/PublicClass.java
@@ -0,0 +1,8 @@
+package com.example.access;
+
+public class PublicClass {
+	public void showPublicMessage() {
+		System.out.println("This is Public class message");
+	}
+
+}

6848959f7cdeab8ce877ec907f2b7e6621a7513f
Syamalan <syamala.nuthalapati@gmail.com>
2025-09-23 17:14:10 +0530

    Basic Package Creation

---
+++ Eclipse/JAVA_Eclipse/src/com/example/math/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.math;
+
+public class Calculator {
+	public void addition() {
+		System.out.println("Addition method");
+	}
+	public void subtraction() {
+		System.out.println("Subtraction method");
+	}
+
+}
